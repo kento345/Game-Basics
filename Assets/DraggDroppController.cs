5ad9da482285d1bc4027258a5135d8706eca6b07
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2025-09-04 22:48:51 +0900

    Dragg&Dropp

---
+++ Assets/DraggDroppController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DraggDroppController : MonoBehaviour
+{
+    private GameObject selectObject = null;
+    private Vector3 offset;
+    private float dragDistance;
+    private float originalY = 1f;
+
+    [SerializeField] private LayerMask Dragglayer;
+    private RaycastHit hit;
+
+    
+    //-----Ray-----
+    private Ray ray;
+
+
+    public void OnMouse1(InputAction.CallbackContext context)
+    {
+        // 左クリック
+        if (context.performed)
+        {
+            Dragg();
+        }
+        // ドラッグ終了
+        if (context.canceled)
+        {
+            selectObject = null;
+        }
+    }
+
+ 
+
+
+    void Update()
+    {
+       ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // ドラッグ中
+        if (selectObject != null)
+        {
+            if (((1 << selectObject.gameObject.layer) & Dragglayer) != 0)
+            {
+                // DraggLayerのオブジェクトは移動
+                Vector3 worldPos = ray.GetPoint(dragDistance);
+                selectObject.transform.position = new Vector3(worldPos.x + offset.x, originalY, worldPos.z + offset.z);
+
+            }
+        }
+    }
+
+    void MoveObject()
+    {
+
+    }
+
+
+    void Dragg()
+    {
+        if(Physics.Raycast(ray,out hit))
+        {
+            if (((1 << hit.collider.gameObject.layer) & Dragglayer) != 0)
+            {
+                selectObject = hit.collider.gameObject;
+                dragDistance = Vector3.Distance(Camera.main.transform.position, selectObject.transform.position);
+                var worldPos = ray.GetPoint(dragDistance);
+                offset       = selectObject.transform.position - worldPos;
+            }
+        }
+    }
+}
